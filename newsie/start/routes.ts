581df66523fea8d94e76563cd0e07e6e61d859c3
unknown <xlmriosx@gmail.com>
2023-11-18 22:59:24 -0300

    path /

--- newsie/start/routes.ts
+++ newsie/start/routes.ts
@@ -20,6 +20,10 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.get('/', async () => {
+  return { message: 'can you check this service with postman' }
+})
+
 Route.get('/health-check', async () => {
   return { status: 'ok' }
 })
