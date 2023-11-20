c7bc7253cd413da48c33de376f0fbee2cf86074a
hadamafer <hadamafer@gmail.com>
2023-11-20 20:48:52 -0300

    dev-fer & dev-fede seeders user

---
+++ newsie/database/seeders/UserSeeder.ts
@@ -0,0 +1,40 @@
+import User from "App/Models/User";
+import { DateTime } from "luxon";
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+
+export default class UserSeeder extends BaseSeeder {
+  public async run() {
+    await User.createMany([
+      {
+        email: "fede@adonisjs.com",
+        password: "secret123",
+        created_at: DateTime.now(),
+        updated_at: DateTime.now(),
+      },
+      {
+        email: "fer@adonisjs.com",
+        password: "secret123",
+        created_at: DateTime.now(),
+        updated_at: DateTime.now(),
+      },
+      {
+        email: "lucas@adonisjs.com",
+        password: "secret123",
+        created_at: DateTime.now(),
+        updated_at: DateTime.now(),
+      },
+      {
+        email: "jorge@adonisjs.com",
+        password: "secret123",
+        created_at: DateTime.now(),
+        updated_at: DateTime.now(),
+      },
+      {
+        email: "facu@adonisjs.com",
+        password: "secret123",
+        created_at: DateTime.now(),
+        updated_at: DateTime.now(),
+      },
+    ]);
+  }
+}
