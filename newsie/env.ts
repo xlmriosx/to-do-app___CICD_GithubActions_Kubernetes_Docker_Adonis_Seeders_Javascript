8788a784ba1c793379387baccaf7abfbfbfb8882
federicomoneta;,federico <fedemos123@gmail.com>
2023-11-29 20:13:02 -0300

    dev-federico - multienvironment added

--- newsie/env.ts
+++ newsie/env.ts
@@ -12,20 +12,20 @@
 |
 */
 
-import Env from '@ioc:Adonis/Core/Env'
+import Env from "@ioc:Adonis/Core/Env";
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
-	APP_NAME: Env.schema.string(),
-    DRIVE_DISK: Env.schema.enum(['local'] as const),
-	NODE_ENV: Env.schema.enum(['development', 'production', 'local'] as const),
-	// Variables added when install `node ace invoke @adonisjs/lucid`
-	DB_CONNECTION: Env.schema.string(),
-	MYSQL_HOST: Env.schema.string({ format: 'host' }),
-    MYSQL_PORT: Env.schema.number(),
-    MYSQL_USER: Env.schema.string(),
-    MYSQL_PASSWORD: Env.schema.string.optional(),
-    MYSQL_DB_NAME: Env.schema.string(),
-})
+  HOST: Env.schema.string({ format: "host" }),
+  PORT: Env.schema.number(),
+  APP_KEY: Env.schema.string(),
+  APP_NAME: Env.schema.string(),
+  DRIVE_DISK: Env.schema.enum(["local"] as const),
+  NODE_ENV: Env.schema.enum(["master", "qa", "development", "local"] as const),
+  // Variables added when install `node ace invoke @adonisjs/lucid`
+  DB_CONNECTION: Env.schema.string(),
+  MYSQL_HOST: Env.schema.string({ format: "host" }),
+  MYSQL_PORT: Env.schema.number(),
+  MYSQL_USER: Env.schema.string(),
+  MYSQL_PASSWORD: Env.schema.string.optional(),
+  MYSQL_DB_NAME: Env.schema.string(),
+});
